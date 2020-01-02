51369ea4449cd19a178ac520bfb9627df6384fb7
一晌欢愉♘ <@DESKTOP-GT8RHKG>
2020-01-02 20:30:27 +0800

    再试一次

--- src/main/java/exam05/exam5.java
+++ src/main/java/exam05/exam5.java
@@ -3,6 +3,6 @@ package exam05;
 public class exam5 {
 
 	public static void main(String[] args) {
-		System.out.println("天天都是Hallo  Hallo   哈喽个锤子啊");
+		System.out.println("天天都是Hallo  Hallo   哈喽个锤子啊        想砍死你");
 	}
 }
